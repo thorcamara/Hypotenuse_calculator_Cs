ed8af2c6dbf4d8eb0a439c02e2091ee76a816899
Thor <102772895+thorcamara@users.noreply.github.com>
2023-07-07 21:44:32 -0300

    Initial commit.

---
+++ Hypotenuse_calculator/Hypotenuse_calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Hypotenuse_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter side A: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter side b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = Math.Sqrt((a * a) + (b * b));
+
+            Console.WriteLine("The hypotenuse is: " + c);
+
+            Console.ReadKey();
+        }
+    }
+}
